16c37637be3a13a8a901500ac9bf19bff00ea675
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2019-06-02 08:47:27 -0500

    remove transition from key

--- drumKit/main.js
+++ drumKit/main.js
@@ -1,4 +1,4 @@
-window.addEventListener('keydown', function (e) {
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
@@ -7,6 +7,16 @@ window.addEventListener('keydown', function (e) {
   audio.play();
 
   key.classList.add('playing');
+}
   
+function removeTransition(e) {
+  if (e.propertyName != 'transform') return;
+  this.classList.remove('playing');
+};
+
+const keys = document.querySelectorAll('.key');
+[...keys].map(key => key.addEventListener('transitionend', removeTransition));
   
-});
+window.addEventListener('keydown', playSound);
+
+
