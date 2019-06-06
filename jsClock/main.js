20e8fe0d40ca8fdc644a5bdd52991227dfba2d60
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2019-06-06 14:08:31 -0500

    create seconds, minutes and hours function

---
+++ jsClock/main.js
@@ -0,0 +1,24 @@
+function setDate() {
+  const secondHand = document.querySelector('.second-hand');
+  const minuteHand = document.querySelector('.min-hand');
+  const hourHand = document.querySelector('.hour-hand');
+
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const minutes = now.getMinutes();
+  const minutesDegrees =  ((minutes / 60) * 360) + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hour = now.getHours();
+  const hourDegrees = ((hour / 12 ) * 360) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+console.log(seconds,  minutes, hour);
+
+  }
+
+setInterval( setDate, 1000);
