e0beffc544e253b2df0a2da18439e9fcca6e0284
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2019-07-04 19:43:02 -0400

    code refactoring

--- arrayCardioDay1/main.js
+++ arrayCardioDay1/main.js
@@ -1,32 +1,33 @@
-    // ## Array Cardio Day 1
-    const inventors = [
-      { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
-      { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
-      { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
-      { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
-      { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
-      { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
-      { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
-      { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
-      { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
-      { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
-      { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
-      { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
-    ];
-    const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry',
-      'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul',
-      'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter',
-      'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas',
-      'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano',
-      'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra',
-      'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin',
-      'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose',
-      'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine',
-      'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William', 'Michael, Zoe'
-    ];
+  // ## Array Cardio Day 1
+  const inventors = [
+    { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+    { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+    { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+    { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+    { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+    { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+    { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+    { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+    { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+    { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+    { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+    { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
+  ];
+  
+  const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 
+    'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul',
+    'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter',
+    'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas',
+    'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano',
+    'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra',
+    'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin',
+    'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose',
+    'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine',
+    'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William', 'Michael, Zoe'
+  ];
     
-    // Array.prototype.filter()
-    // 1. Filter the list of inventors for those who were born in the 1500's
+  // Array.prototype.filter()
+  // 1. Filter the list of inventors for those who were born in the 1500's
    const bornInthe1500s = inventors.filter(inventor => (inventor.year > 1500 && inventor.year < 1600));
    console.table(bornInthe1500s);
    
@@ -51,7 +52,7 @@
   console.log(totalInventorsYears);
     
   // 5. Sort the inventors by years lived
-  const oldestInventor = inventors.sort(function(a, b) {
+  const oldestInventor = inventors.sort((a, b) => {
     const lastGuy = a.passed - a.year;
     const nextGuy = b.passed - b.year;   
     return lastGuy > nextGuy ? -1 : 1;
@@ -60,7 +61,6 @@
   console.table(oldestInventor);
    
   // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
-  // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
   const boulevards = ['Boulevards de Paris', 'City walls de Paris', 'Thiers wall', 'Wall de Charles V',
     'Wall de Philip II Augustus', 'City gates de Paris', 'Haussmann\'s renovation de Paris',
     'Boulevards de the Marshals', 'Boulevard Auguste-Blanqui', 'Boulevard Barbès', 
