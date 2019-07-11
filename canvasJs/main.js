9e0c4a1a441f4a48d05d6a364d9b8e8c665d953f
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2019-07-11 17:49:12 -0400

    create color picker

--- canvasJs/main.js
+++ canvasJs/main.js
@@ -1,10 +1,22 @@
 const canvas = document.querySelector('#draw');
+const input = document.querySelector('input');
 
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-ctx.stokeStyle = 'BADA55';
+const color = input.value;
+
+function colorPalete() {
+  console.log(this.value);
+  
+  ctx.strokeStyle = this.value;
+  console.log('jiiii');
+  
+ 
+}
+// ctx.strokeStyle = color;
+
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 
@@ -16,7 +28,6 @@ let lastY = 0;
 function draw(e) {
   
   if(!isDrawing) return; // stop from running when theres not mousedown
-  console.log(e);
   ctx.beginPath();
   ctx.moveTo(lastX, lastY);
   ctx.lineTo(e.offsetX, e.offsetY);
@@ -34,3 +45,5 @@ canvas.addEventListener('mousemove', draw);
 
 canvas.addEventListener('mouseup', () => isDrawing = false );
 canvas.addEventListener('mouseout', () => isDrawing = false );
+input.addEventListener('change', colorPalete);
+input.addEventListener('mousemove', colorPalete);
