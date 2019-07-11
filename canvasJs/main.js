69afab55bdec845b608bfb648c530d7fa6552292
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2019-07-11 16:21:49 -0400

    create function to draw

--- canvasJs/main.js
+++ canvasJs/main.js
@@ -17,9 +17,20 @@ function draw(e) {
   
   if(!isDrawing) return; // stop from running when theres not mousedown
   console.log(e);
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
 }
 
+canvas.addEventListener('mousedown', (e) =>  {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+
+});
+
 canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', () => isDrawing = true );
+
 canvas.addEventListener('mouseup', () => isDrawing = false );
 canvas.addEventListener('mouseout', () => isDrawing = false );
