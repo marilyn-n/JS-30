270e1447cee4a5ae33e8122148cca5f5c00bdd0d
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2019-07-18 14:53:09 -0400

    image scrolling

--- slideInOnScroll/main.js
+++ slideInOnScroll/main.js
@@ -1,4 +1,4 @@
-function debounce(func, wait = 20, immediate = true) {
+function debounce(func, wait = 100, immediate = true) {
   var timeout;
   return function() {
     var context = this, args = arguments;
@@ -12,3 +12,28 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+const sliderImages = document.querySelectorAll('.slide-in')
+
+
+function checkSlide(e) {
+  [...sliderImages].map(sliderImage => {
+    // half way through the image
+    const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2 ;
+    // bottom of the image
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    // half of the image is shown?
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add('active');
+    } else {
+      sliderImage.classList.remove('active');
+    }
+
+  });
+}
+
+
+window.addEventListener('scroll', debounce(checkSlide));
