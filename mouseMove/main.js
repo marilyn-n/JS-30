d9a1881ff23bc612a30f1af789715258ca507bb1
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2019-07-19 18:42:36 -0400

    shadow function

---
+++ mouseMove/main.js
@@ -0,0 +1,27 @@
+const hero = document.querySelector('.hero');
+const text = hero.querySelector('h1');
+const walk = 100; // 100px
+
+const shadow = (e) => {
+    const {
+        offsetWidth: width,
+        offsetHeght: height
+    } = hero;
+
+    let {
+        offsetX: x,
+        offsetY: y
+    } = e; // get cursor position
+
+    if (this !== e.target) {
+        x = x + e.target.offsetLeft;
+        y = y + e.target.offsetTop;
+    }
+
+    const xWalk = Math.round((x / width * walk) - (walk / 2));
+    const yWalk = Math.round((y / height * walk) - (walk / 2));
+
+    text.style.textShadow = `${xWalk}px ${yWalk}px 0 red `
+}
+
+hero.addEventListener('mousemove', shadow);
