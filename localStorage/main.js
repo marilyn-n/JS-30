ef8888b0afdd976e0751ef84a66b6cb8a307e8c3
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2019-07-19 15:22:14 -0400

    populate item list

--- localStorage/main.js
+++ localStorage/main.js
@@ -9,8 +9,21 @@ function addItem(e) {
         text,
         done: false
     }
+    items.push(item);
+    populateList(items, itemsList);
     this.reset();
-    console.log(item);
+}
+
+
+function populateList(plates = [], platesList) {
+    platesList.innerHTML = plates.map((plate, i) => {
+        return `
+            <li>
+                <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked': '' }>
+                <label for="item${i}">${plate.text}</label>
+            </li>
+        `;
+    }).join('');
 
 }
 
