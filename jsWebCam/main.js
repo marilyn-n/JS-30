0f0168c37531588636d44ff0aea76840302c4738
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2019-07-21 15:55:10 -0400

    add efects

--- jsWebCam/main.js
+++ jsWebCam/main.js
@@ -14,9 +14,6 @@ const getVideo = () => {
         .catch(err => console.error('OHHH NOOO', err));
 }
 
-getVideo();
-
-
 const painToCanvas = () => {
     const width = video.videoWidth;
     const height = video.videoHeight;
@@ -24,6 +21,11 @@ const painToCanvas = () => {
     canvas.height = height;
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
+        let pixels = ctx.getImageData(0, 0, width, height);
+        // pixels = redEffect(pixels);
+        pixels = rgbSplit(pixels);
+        // pixels = greenScreen(pixels);
+        ctx.putImageData(pixels, 0, 0);
     }, 16);
 }
 
@@ -40,5 +42,53 @@ const takePhoto = () => {
     link.innerHTML = `<img src="${data}" alt="pic">`;
     strip.insertBefore(link, strip.firstChild);
 }
+
+const redEffect = (pixels) => {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 100; //RED
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; // GEEN
+        pixels.data[i + 2] = pixels.data[i + 2] + .5; // BLUE
+    }
+    return pixels;
+}
+
+const rgbSplit = (pixels) => {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i - 150] = pixels.data[i + 0]; //RED
+        pixels.data[i + 500] = pixels.data[i + 1]; // GEEN
+        pixels.data[i - 550] = pixels.data[i + 2]; // BLUE
+    }
+    return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value;
+    });
+
+    for (i = 0; i < pixels.data.length; i = i + 4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+
+        if (red >= levels.rmin &&
+            green >= levels.gmin &&
+            blue >= levels.bmin &&
+            red <= levels.rmax &&
+            green <= levels.gmax &&
+            blue <= levels.bmax) {
+            // take it out!
+            pixels.data[i + 3] = 0;
+        }
+    }
+
+    return pixels;
+}
+
+
+getVideo();
 
 video.addEventListener('canplay', painToCanvas);
