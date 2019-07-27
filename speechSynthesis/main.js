0f2a03edf187ee863d8cc6668fd66a8114a78749
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2019-07-27 19:21:07 -0400

    change language and toggle speaking

--- speechSynthesis/main.js
+++ speechSynthesis/main.js
@@ -4,3 +4,28 @@ const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
+msg.text = document.querySelector('[name="text"]').value;
+console.log(msg.text);
+
+function populateVoices() {
+    voices = this.getVoices();
+    voicesDropdown.innerHTML = voices.map(voice =>
+        ` <option value="${voice.name}">${voice.name}(${voice.lang})</option>`
+    ).join('');
+
+}
+
+function setVoice() {
+    msg.voice = voices.find(voice => voice.name === this.value);
+    toggle();
+}
+
+function toggle(startOver = true) {
+    speechSynthesis.cancel();
+    if (startOver) {
+        speechSynthesis.speak(msg);
+    }
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+voicesDropdown.addEventListener('change', setVoice);
