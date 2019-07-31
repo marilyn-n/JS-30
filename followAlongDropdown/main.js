757928f4a07183a8f9a5e66e9fa4ed52189e8eb9
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2019-07-30 22:04:39 -0400

    fix navbar

--- followAlongDropdown/main.js
+++ followAlongDropdown/main.js
@@ -6,7 +6,12 @@ const nav = document.querySelector('.top');
 function handleEnter() {
     console.log('enter');
     this.classList.add('trigger-enter');
-    setTimeout(() => this.classList.add('trigger-enter-active'), 150);
+    setTimeout(() => {
+        if (this.classList.contains('trigger-enter')) {
+            this.classList.add('trigger-enter-active')
+        }
+    }, 150);
+
     background.classList.add('open');
 
     const dropdown = this.querySelector('.dropdown');
@@ -16,13 +21,13 @@ function handleEnter() {
     const coords = {
         height: dropCoords.height,
         width: dropCoords.width,
-        top: navCoords.top,
-        left: navCoords.left,
+        top: dropCoords.top - navCoords.top,
+        left: dropCoords.left - navCoords.left,
     }
 
     background.style.setProperty('width', `${coords.width}px`);
     background.style.setProperty('height', `${coords.height}px`);
-    // background.style.translate(`transform(${coords.top}px, ${coords.left}px)`);
+    background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`);
 
 }
 
