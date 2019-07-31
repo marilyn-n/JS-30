25949f508f249c4a3551b8edcc1b0d29d40cab71
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2019-07-30 21:52:57 -0400

    get heigh and width depending on dropdown size

--- followAlongDropdown/main.js
+++ followAlongDropdown/main.js
@@ -7,11 +7,29 @@ function handleEnter() {
     console.log('enter');
     this.classList.add('trigger-enter');
     setTimeout(() => this.classList.add('trigger-enter-active'), 150);
+    background.classList.add('open');
+
+    const dropdown = this.querySelector('.dropdown');
+    const dropCoords = dropdown.getBoundingClientRect();
+    const navCoords = nav.getBoundingClientRect();
+
+    const coords = {
+        height: dropCoords.height,
+        width: dropCoords.width,
+        top: navCoords.top,
+        left: navCoords.left,
+    }
+
+    background.style.setProperty('width', `${coords.width}px`);
+    background.style.setProperty('height', `${coords.height}px`);
+    // background.style.translate(`transform(${coords.top}px, ${coords.left}px)`);
+
 }
 
 function handleLeave() {
     console.log('leave');
-    this.classList.remove('trigger-enter');
+    this.classList.remove('trigger-enter', 'tringger-enter-active');
+    background.classList.remove('open');
 
 }
 
